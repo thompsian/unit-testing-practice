f4ae83f85b05ad1a229da96cc0d88a2573ce31e7
thompsian <thompsian@gmail.com>
2021-11-01 13:52:39 -0700

    additional matcher

--- main.js
+++ main.js
@@ -1,55 +1,28 @@
-/*  Unit Testing: Mocks Challenge
+/* Unit Testing: Additional Matchers Challenge
 *
-*   1. Clean up the test below and remove unused pieces
-*   2. Write a test that test getMyFullUserData gets the provided user data
+* 1. Look through the docs and write a test for a matcher we haven't discussed yet
+* - https://jasmine.github.io/api/edge/matchers
 */
 
 // Test Suite 
 describe(`${User.name} Class`, () => {
     let model;
-    let mockUserService;
     
     beforeEach(() => {
-        mockUserService = {
-            lastId: null,
-            user: {},
-            getUserById(id) {
-                this.lastId = id;
-                
-                return this.user;
-            }
-        };
-        const data = { firstName: 'Dylan', middleName: 'Christopher', lastName: 'Israel', id: 1 };
-        model = new User(data, mockUserService);
+        model = new User();
     });
 
-    describe('getMyFullUserData', () => {
-        it('passes id to get user', async () => {
+    describe('additional matchers testing area', () => {
+        it('has my first name', () => {
             // arrange
-            mockUserService.lastId = null;
-
-            // act
-            await model.getMyFullUserData();
-            
-            // assert
-            expect(mockUserService.lastId).toBe(1);
-        });
-        
-        it('gets full user data', async () => {
-            // arrange
-             mockUserService.user = new User({ 
-                    firstName: 'Dollan', 
-                    middleName: 'Coding Go', 
-                    lastName: 'Noneya', 
-                    id: 2 
-            });
+            const firstName = 'Dylan';
+            const lastName = 'Israel';
             
             // act
-            const result = await model.getMyFullUserData();
+            model = new User({firstName, lastName});
             
             // assert
-            expect(result.id).toBe(2);
-            expect(result.firstName).toBe('Dollan');
+            expect(model.fullName).toMatch(/Dylan/);
         });
     });
 });
--- user.js
+++ user.js
@@ -2,15 +2,11 @@ class User {
     firstName;
     lastName;
     middleName;
-    id;
-    userService;
     
-    constructor(data, userService){
+    constructor(data = {}){
         this.firstName = data.firstName || '';
         this.lastName = data.lastName || '';
         this.middleName = data.middleName || '';
-        this.id = data.id;
-        this.userService = userService;
     }
         
     get fullName() {
@@ -20,23 +16,9 @@ class User {
         
         return `${this.firstName} ${this.lastName}`;
     }
-    
-    async getMyFullUserData() {
-        return this.userService.getUserById(this.id);
-    }
-    
-    sayMyName() {
-        alert(this.fullName);
-    }
-    
-    getCodeName() {
-        const isATestingGod = confirm('Are you a testing god?');
-        
-        if(isATestingGod) {
-            return 'TESTING GOD!'
-        } else {
-            return `Scrub skipping tests in his best friend's ride!`;
-        }
+
+    get fullNamePieces() {
+        return [this.firstName, this.middleName, this.lastName];
     }
 } 
  
